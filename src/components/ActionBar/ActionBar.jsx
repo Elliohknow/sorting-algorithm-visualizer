3c8fe142bd84e74564a97c5f79cd4ffb17fed44e
Elliott <elliottohj44@gmail.com>
2020-04-19 06:26:57 -0400

    replaced custom component with material-ui Button component

--- src/components/ActionBar/ActionBar.jsx
+++ src/components/ActionBar/ActionBar.jsx
@@ -1,38 +1,33 @@
 import Button from '@material-ui/core/Button';
 import React from 'react';
-import { CTX } from '../../Store';
+import {CTX} from '../../Store';
 import './ActionBar.css';
-import RadioButton from './RadioButton';
 
 export default function ActionBar() {
-	const { state, dispatch } = React.useContext(CTX);
+  const {state, dispatch} = React.useContext(CTX);
 
-	return (
-		<div className="">
-			<form
-				className="action-bar"
-				onSubmit={(e) => {
-					e.preventDefault();
-				}}
-			>
-				<Button
-					id="reset-array"
-					color="primary"
-					variant="contained"
-					className="button"
-					onClick={() => dispatch({type: 'reset'})}
-				>reset array</Button>
-				{state.options.map((option, index) => (
-					<RadioButton
-						dispatch={dispatch}
-						key={`radio_button_${index+1}`}
-						name={option.name}
-						value={option.value}
-						color={`${state.algorithm !== option.value ? 'primary' : 'secondary'}`}
-						// className={`button ${state.algorithm !== option.value ? 'off': 'on'}`}
-					/>
-				))}
-			</form>
-		</div>
-	);
+  return (
+    <div className="action-bar">
+      <Button
+        id="reset-array"
+        color="primary"
+        variant="contained"
+        className="button"
+        onClick={() => dispatch({type: 'reset'})}>
+        reset array
+      </Button>
+      {state.options.map((option, index) => (
+        <Button
+          variant="contained"
+          className="button"
+          onClick={() => dispatch({type: option.value})}
+          key={`radio_button_${index + 1}`}
+          name={option.name}
+          value={option.value}
+          color={`${state.algorithm !== option.value ? 'primary' : 'secondary'}`}>
+          {option.value} sort
+        </Button>
+      ))}
+    </div>
+  );
 }
--- src/components/Visualizer/Visualizer.jsx
+++ src/components/Visualizer/Visualizer.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import './Visualizer.css';
 
 export default function Visualizer(props) {
-	const { array } = props;
+  const {array} = props;
 
-	return (
-		<main className="container">
-			{array.map((value, index) => (
-				<div className="element" key={`element_${index}`} title={value} style={{ height: value*3}} />
-			))}
-		</main>
-	);
+  return (
+    <main className="container">
+      {array.map((value, index) => (
+        <div
+          className="element"
+          key={`element_${index}`}
+          title={value}
+          style={{height: value * 3}}
+        />
+      ))}
+    </main>
+  );
 }
